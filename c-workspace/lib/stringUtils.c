4598c39afff30ef10de3f75d54ed8675ce3edc0c
Patrick Junker <paju2406@gmx.de>
2023-06-14 18:22:12 +0200

    difseeo

--- c-workspace/lib/stringUtils.c
+++ c-workspace/lib/stringUtils.c
@@ -18,27 +18,38 @@ string_list_t* split(char* input, char* delimiter){
   int temp_index = 0;
   string_list_t* list = create_list();
   while (*input != '\0') {
+    printf("%c\n", *input);
     if(*input != *temp_delimiter){
+      printf("ping%c\n",  *input);
       temp[temp_index] = *input;
       temp_index++;
       input++;
     } else {
+      printf("pong%c\n",  *input);
       char temp2[10];
       int temp_index2 = 0;
-      while(*temp_delimiter != '\0')  
-        if(*input == *temp_delimiter){
-          temp2[temp_index2] = *input;
-          input++;
-        }
-        
-        printf("%c\n", *input);
-        
+      int failed = 0;
+      while(*temp_delimiter != '\0'){
+        temp2[temp_index2] = *input;
         input++;
+        temp_index2++;
+        if(*input != *temp_delimiter){
+          failed = 1;
+          break;
+        }
+
+      } 
+      if(failed){
+        strcat(temp,temp2);
+      }else{
+        char* peter = (char*) malloc(temp_index2 + 1);
+        strcpy(peter, temp);
+        memset(&temp[0], 0, sizeof(temp));
+        temp_delimiter = delimiter;
+        temp_index = 0;
+        printf("%s\n", peter);
+      } 
     }
-
-    
-
-
     return list;
   }
 }
