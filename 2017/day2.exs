0cb3519ecf2b7845e9d4d3139e020b3188eb1cbd
Patrick Junker <paju2406@gmx.de>
2024-07-30 21:23:27 +0200

    2017 elixir day2

---
+++ 2017/day2.exs
@@ -0,0 +1,30 @@
+defmodule Part_one do
+  def solve(input) do
+    Enum.reduce(input, 0, fn line, acc -> acc + (Enum.max(line) - Enum.min(line)) end)
+  end
+end
+
+defmodule Part_two do
+  def solve(input) do
+    Enum.reduce(input, 0, fn line, acc -> acc + divison_rec(Enum.sort(line, :desc)) end)
+  end
+
+  def divison_rec([head | tail]) do
+    peter = Enum.find(tail, fn x -> rem(head, x) == 0 end)
+
+    if(peter !== nil) do
+      div(head, peter)
+    else
+      divison_rec(tail)
+    end
+  end
+end
+
+input =
+  File.read!("input.txt")
+  |> String.split("\n")
+  |> Enum.map(fn line -> String.split(line, "\t") end)
+  |> Enum.map(fn line -> Enum.map(line, fn x -> String.to_integer(x) end) end)
+
+IO.puts("Part one: #{Part_one.solve(input)}")
+IO.puts("Part two: #{Part_two.solve(input)}")
